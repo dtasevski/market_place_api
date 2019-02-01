929542edd04b975fec12808951e2986b1abb7546
dtasevski <duskotasevski@hotmail.com>
2019-02-01 16:01:41 +0100

    Add first User specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,11 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'shoulda/matchers'
+require 'capybara/rspec'
+require 'email_spec'
 # Add additional requires below this line. Rails is not loaded until this point!
-#require 'support/factory_bot'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -20,7 +23,7 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -58,4 +61,20 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  # config.include Devise::Test::ControllerHelpers, type: :controller
+  # config.include Devise::TestHelpers, :type => :controller
+  # config.include Request::JsonHelpers, :type => :controller
+  # config.include Request::HeadersHelpers, :type => :controller
+  # config.before(:each, type: :controller) do
+  #   include_default_accept_headers
+  # end
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
