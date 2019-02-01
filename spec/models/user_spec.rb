05a26df99aedadce3c5d95552fdb5ac3d7fb90f4
dtasevski <duskotasevski@hotmail.com>
2019-02-01 16:06:21 +0100

    Add shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,9 @@ RSpec.describe User, type: :model do
   it { should respond_to (:password) }
   it { should respond_to(:password_confirmation) }
   it { should be_valid}
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_presence_of(:password_confirmation) }
+  it { should allow_value("example@domain.com").for(:email) }
 end
