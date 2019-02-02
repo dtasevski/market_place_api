4a93fdc7f2bdc328129fb153d8f52204947a622d
dtasevski <duskotasevski@hotmail.com>
2019-02-02 16:05:47 +0100

    Add session create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :auth_token, uniqueness: true
-
+  before_create :generate_authentication_token!
   def generate_authentication_token!
     begin
       self.auth_token = Devise.friendly_token
