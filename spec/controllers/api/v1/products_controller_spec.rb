ca7baa6fa2d7a771cba53ec722a704c38e1e44c5
dtasevski <duskotasevski@hotmail.com>
2019-02-03 13:55:56 +0100

    Add show and index product endpoint

---
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ProductsController, type: :controller do
+
+  describe "GET #show" do
+    before(:each) do
+      @product = FactoryBot.create :product
+      get :show, params: { id: @product.id }
+    end
+
+    it "returns information about a reporter on a hash" do
+      expect(json_response[:title]).to eql @product.title
+    end
+
+    it { should respond_with 200 }
+  end
+
+  describe "GET #index" do
+    before(:each) do
+      4.times { FactoryBot.create :product }
+      get :index
+    end
+
+    it "returns 4 records from the database" do
+      expect(json_response[:products].length).to eq(4)
+    end
+
+    it { should respond_with 200 }
+  end
+
+end
