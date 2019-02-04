0fdf3585aafe44477e58ccfa2367f757e4b7dbc3
dtasevski <duskotasevski@hotmail.com>
2019-02-04 12:25:29 +0100

    Add create, update, destroy actions on product resources

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -28,4 +28,89 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
     it { should respond_with 200 }
   end
 
+  describe "POST #create" do
+    context "when is successfully created" do
+      before(:each) do
+        user = FactoryBot.create :user
+        @product_attributes = FactoryBot.attributes_for :product
+        api_authorization_header user.auth_token
+        post :create, params: { user_id: user.id, product: @product_attributes}
+      end
+
+      it "renders the json representation of the product record just created" do
+        expect(json_response[:product][:title]).to eql @product_attributes[:title]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        user = FactoryBot.create :user
+        @invalid_product_attributes = { title: "Smart TV", price: "Twelve Dollars" }
+        api_authorization_header user.auth_token
+        post :create, params: { user_id: user.id, product: @invalid_product_attributes }
+      end
+
+      it "renders an error json" do
+        expect(json_response).to have_key(:errors)
+      end
+
+      it "renders the json on why the product could not be created" do
+        expect(json_response[:errors][:price]).to include "is not a number"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
+  describe "PATCH #update" do
+    before(:each) do
+      @user = FactoryBot.create :user
+      @product = FactoryBot.create :product, user: @user
+      api_authorization_header @user.auth_token
+    end
+
+    context "when is successfully updated" do
+      before(:each) do
+        patch :update, params: { user_id: @user.id, id: @product.id,
+                                        product: { title: "An Expensive TV" } }
+      end
+
+      it "renders the json representation for the updated product" do
+        expect(json_response[:product][:title]).to eql "An Expensive TV"
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when is not updated" do
+      before(:each) do
+        patch :update, params: { user_id: @user.id, id: @product.id,
+                                        product: { price: "Two Hundred" } }
+      end
+
+      it "renders an error json" do
+        expect(json_response).to have_key(:errors)
+      end
+
+      it "renders the json error message on why the product could not be updated" do
+        expect(json_response[:errors][:price]).to include "is not a number"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before(:each) do
+      @user = FactoryBot.create :user
+      @product = FactoryBot.create :product, user: @user
+      api_authorization_header @user.auth_token
+      delete :destroy, params: { user_id: @user.id, id: @product.id }
+    end
+
+    it { should respond_with 204 }
+  end
+
 end
