0f4676cfa6399200c0a1da2876bfc401f976f370
dtasevski <duskotasevski@hotmail.com>
2019-02-05 22:34:17 +0100

    Add search engine

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,31 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   let(:product) { FactoryBot.build :product}
-
   subject { product }
 
-  it { should respond_to(:title) }
-  it { should respond_to(:price) }
-  it { should respond_to(:published) }
-  it { should respond_to(:user_id) }
-  it { should be_valid }
-  it { should_not be_published }
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:price) }
-  it { should validate_presence_of(:user_id) }
-  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
-  it { should belong_to :user }
+  it { should respond_to :title }
+  it { should respond_to :price }
+  it { should respond_to :published }
+  it { should respond_to :user_id }
+  it { should_not be_published}
+  it { should validate_presence_of :title}
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0)}
+  it { should validate_presence_of :user_id}
+  it { should belong_to :user}
 
   describe ".filter_by_title" do
     before(:each) do
-      @product1 = FactoryBot.create :product, title: "A Plasma TV"
+      @product1 = FactoryBot.create :product, title: "A plasma TV"
       @product2 = FactoryBot.create :product, title: "Fastest Laptop"
-      @product3 = FactoryBot.create :product, title: "CD Player"
+      @product3 = FactoryBot.create :product, title: "CD player"
       @product4 = FactoryBot.create :product, title: "LCD TV"
     end
 
-    context "when a 'TV' title pattern is set" do
-      it "returns 2 products matching" do
+    context "when a TV title pattern is set" do
+      it "returns the 2 products matching" do
         expect(Product.filter_by_title("TV").count).to eq(2)
       end
 
@@ -44,7 +41,7 @@ RSpec.describe Product, type: :model do
       @product4 = FactoryBot.create :product, price: 99
     end
 
-    it "returns the products that are equal or above to given price" do
+    it "returns the products that are above or equal to given price" do
       expect(Product.above_or_equal_to_price(100).sort).to match_array([@product1, @product3])
     end
   end
@@ -57,8 +54,8 @@ RSpec.describe Product, type: :model do
       @product4 = FactoryBot.create :product, price: 99
     end
 
-    it "returns the products that are below or equal to given price" do
-      expect(Product.below_or_equal_to_price(100).sort).to match_array([@product1, @product2, @product4])
+    it "returns the products that are below or equal to price" do
+      expect(Product.below_or_equal_to_price(99).sort).to match_array([@product2, @product4])
     end
   end
 
@@ -68,13 +65,49 @@ RSpec.describe Product, type: :model do
       @product2 = FactoryBot.create :product, price: 50
       @product3 = FactoryBot.create :product, price: 150
       @product4 = FactoryBot.create :product, price: 99
-
       @product2.touch
       @product3.touch
     end
 
-    it "returns the most updated products" do
+    it "returns the most recent updated records" do
       expect(Product.recent).to match_array([@product3, @product2, @product4, @product1])
+    end
+  end
+
+  describe ".search" do
+    before(:each) do
+      @product1 = FactoryBot.create :product, price: 100, title: "Plasma tv"
+      @product2 = FactoryBot.create :product, price: 50, title: "Videogame console"
+      @product3 = FactoryBot.create :product, price: 150, title: "MP3"
+      @product4 = FactoryBot.create :product, price: 99, title: "Laptop"
+    end
+
+    context "when title 'videogame' and min '100' price are set" do
+      it "returns an empty array" do
+        search_hash = { keyword: "videogame", min_price: 100 }
+        expect(Product.search(search_hash)).to be_empty
+      end
+    end
+
+    context "when title 'tv', min_price '50' and max_price '100' are set" do
+      it "returns product 1 " do
+        search_hash = { keyword: "tv", min_price: 50, max_price: 100}
+        expect(Product.search(search_hash)).to match_array([@product1])
+      end
+    end
+
+    context "when an empty hash is set" do
+      it "returns all the products" do
+        search_hash = {}
+        expect(Product.search(search_hash)).to match_array([@product1, @product2, @product3, @product4])
+      end
+    end
+
+    context "when product_ids is present" do
+      it "returns the product from the ids" do
+        search_hash = { product_ids: [@product1.id, @product2.id]}
+        expect(Product.search(search_hash)).to match_array([@product1, @product2])
+      end
     end
   end
 end
