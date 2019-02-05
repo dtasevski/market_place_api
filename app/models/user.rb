22a2961e29e57f19714bbcf8716311f3f56f6d17
dtasevski <duskotasevski@hotmail.com>
2019-02-05 23:34:49 +0100

    Adds user order has many relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :auth_token, uniqueness: true
   has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
   before_create :generate_authentication_token!
   def generate_authentication_token!
     begin
